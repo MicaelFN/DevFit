838786cafb2e804e7dbb2f4e84dd1cd49e39f3f8
MicaelFN <micael.f.noronha@gmail.com>
2020-05-03 23:00:57 -0300

    Aula 4

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './src/store';
-import {Text} from 'react-native'
+import {Text} from 'react-native';
+import MainStack from './src/navigators/MainStack';
 
 //importar o Stack
+
 
 export default () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Text>...</Text>
+      
     </PersistGate>
   </Provider>
 )
